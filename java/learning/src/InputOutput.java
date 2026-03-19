b4a453b1ab63e5d79c8c9a88481d2e7b26aeaff6
Harish Puri <harishpuri.in@gmail.com>
2026-03-19 15:08:56 +0530

    Learning Java

---
+++ java/learning/src/InputOutput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class InputOutput {
+
+    static void LogMyName(String userName){
+        System.out.println("This is " + userName);
+    }
+    public static void main(String[] args){
+        LogMyName("Harish");
+    }
+}
